50b02c0c4ee8b5c6e659a468aa5804927f56062a
Krzysztof Sliwinski <kpsliwinski@gmail.com>
2018-03-04 06:10:05 +0100

    Corrected - add comment function

--- src/CommentsListContainer.js
+++ src/CommentsListContainer.js
@@ -1,8 +1,13 @@
 import {connect} from 'react-redux';
 import CommentsList from './CommentsList';
+import {addComment} from './actions.js';
 
 const mapStateToProps = state => ({
   comments: state.comments
 });
 
-export default connect(mapStateToProps)(CommentsList);
+const mapDispatchToProps = dispatch => ({
+  addComment: (comment) => dispatch(addComment(comment))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CommentsList);
